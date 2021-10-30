0af9ead6d2d6f64a38e9d57efdbbdb3bd2a7b551
Stas <liesnychevskyi@yahoo.com>
2021-10-30 15:03:41 +0300

    Commit number : 9
    VerificationHelper created

---
+++ src/main/java/selenium_core/helpers/assertion_helper/VerificationHelper.java
@@ -0,0 +1,69 @@
+package selenium_core.helpers.assertion_helper;
+
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import selenium_core.helpers.logger_helper.LoggerHelper;
+
+public class VerificationHelper
+{
+    //----------------------------------------------------------------------------------------------------------------||
+    private WebDriver driver;
+    private Logger log = LoggerHelper.getLogger(selenium_core.helpers.assertion_helper.VerificationHelper.class);
+    //----------------------------------------------------------------------------------------------------------------||
+    public VerificationHelper(WebDriver driver)
+    {
+        log.info("VerificationHelper constructor is initialized..");
+        this.driver = driver;
+    }
+    //----------------------------------------------------------------------------------------------------------------||
+    public boolean isDisplayed(WebElement element)
+    {
+        try
+        {
+            element.isDisplayed();
+            log.info("Element is present >>> " + element.getText());
+            return true;
+        }
+        catch (Exception e)
+        {
+            log.error("Error, element is not present >>> ", e.getCause());
+            return false;
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------||
+    public boolean isNotDisplayed(WebElement element)
+    {
+        try
+        {
+            element.isDisplayed();
+            log.info(" Error :: Element is displayed >>> " + element.getText());
+            return false;
+        }
+        catch (Exception e)
+        {
+            log.error("Element is not displayed >>> ");
+            return true;
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------||
+    public String getText(WebElement element)
+    {
+        if(null == element)
+        {
+            log.info("WebElement is null.. ");
+            return null;
+        }
+        boolean status = isDisplayed(element);
+        if(status)
+        {
+            log.info("Element text is : " + element.getText());
+            return element.getText();
+        }
+        else
+        {
+            return null;
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------||
+}
